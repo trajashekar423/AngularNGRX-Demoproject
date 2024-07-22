58f33a0e95e4ccfb4453be9d152952395ac2e06a
trajashekar423 <trajashekar423@gmail.com>
2024-07-22 10:58:42 +0530

    homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HerobannerComponent } from "./herobanner/herobanner.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, HeaderComponent,FooterComponent, HerobannerComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HerobannerComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
